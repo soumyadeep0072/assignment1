82388ec3046fdbf678f6887ee7d1878577deae24
soumyadeep0072 <soumyadeepkbanick@gmail.com>
2025-10-07 14:17:49 +0530

    Initial submission: Added assignment files and README

---
+++ assignment_1.py
@@ -0,0 +1,16 @@
+#Assignment 1
+
+#TASK 1
+a = input('Enter the first number: ')
+b = input('Enter the second number: ')
+a = int(a)
+b = int(b)
+print('Addition: ', a+b)
+print('Subtraction: ', a-b)
+print('Multiplication: ', a*b)
+print('Division: ', a/b)
+
+#TASK 2
+c = input('Enter your first name: ')
+d = input('Enter your second name: ')
+print('Hello,',c,d +'! Welcome to the python program.')
